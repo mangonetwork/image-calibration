a8e6df528201703f10d7c5928d6beb2df9c0a534
Leslie Lamarche <leslie.lamarche@sri.com>
2025-08-13 13:42:23 -0700

    Updates/repairs to manual starcal routine.

--- src/mangonetwork/calibrate/starcal.py
+++ src/mangonetwork/calibrate/starcal.py
@@ -19,16 +19,20 @@ import io
 import logging
 import os
 import sys
+import requests
 
 import h5py
 import numpy as np
+
+# Workaround due to bug in matplotlib event handling interface
+# https://github.com/matplotlib/matplotlib/issues/30419
+import matplotlib
+if matplotlib.get_backend() == 'macosx':
+    matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
-from mpl_point_clicker import clicker
 
 from skyfield.api import Star, load, wgs84
 from skyfield.data import hipparcos
-
-from . import imageops
 
 if sys.version_info < (3, 9):
     import importlib_resources as resources
@@ -39,32 +43,46 @@ else:
 class StarCal:
     """Star calibration"""
 
-    def __init__(self, star_cal_file, output):
+    def __init__(self, image_file, output_file):
 
-        self.display_image(star_cal_file)
-        self.lookup_stars()
-        self.save_starcal_file(output, star_cal_file)
+        image = self.load_image(image_file)
+        self.find_stars(image)
+        self.save_starcal_file(output_file)
 
-    def add_star(self, position, klass):
-        x, y = position
+    def add_star(self, click):
+        """Add user selected star and az,el based on HIP."""
+        # This makes use of the Hipparcos Catolog
+        # https://rhodesmill.org/skyfield/stars.html
+
+        # Star location in figure from click event
+        x = click.xdata
+        y = click.ydata
         print(f"Star at {x=:02f}, {y=:02f}")
+
+        # User entered HIP number
         hip = input('HIP #: ')
+        print(hip)
+
+        # Look up star based on HIP and calculate az/el
+        try:
+            s = Star.from_dataframe(self.df.loc[float(hip)])
+        except KeyError:
+            print(f'Entered Hipparcos designation {hip} is not in database!')
+            return
+
+        elev, azmt, _ = self.site_ref.observe(s).apparent().altaz()
+
+        # Append star information
+        self.star_azel.append([azmt.degrees, elev.degrees])
+        self.star_pos.append([x, y])
         self.star_hip.append(hip)
 
-    def prep_image(self, image, contrast=99.95, rotation_angle=0.):
-        """Prepare image to display"""
+        # Mark star on plot
+        self.ax.scatter(x, y, facecolors='none', edgecolors='r')
+        self.fig.canvas.draw()
 
-        cooked_image = np.array(image)
-        cooked_image = imageops.equalize(cooked_image, contrast)
-        cooked_image = imageops.rotate(cooked_image, rotation_angle)
-
-        return cooked_image
-
-
-    def display_image(self, star_cal_file):
-        """Display image and set up klicker"""
-
-        az, el, i, j, raw_file = self.parse_file(star_cal_file)
+    def load_image(self, raw_file):
+        """Load image and metadata from raw file"""
 
         image = h5py.File(raw_file, 'r')['image']
         cooked_image = self.prep_image(image)
@@ -72,53 +90,67 @@ class StarCal:
         self.time = dt.datetime.utcfromtimestamp(image.attrs['start_time'])
         self.site_lat = image.attrs['latitude']
         self.site_lon = image.attrs['longitude']
+        self.site_station = image.attrs['station']
+        self.site_instrument = image.attrs['instrument']
+
+        return cooked_image
+
+    def prep_image(self, image, contrast=99.95, rotation_angle=0.):
+        """Prepare image to display"""
+
+        cooked_image = np.array(image)
+        cooked_image = equalize(cooked_image, contrast)
+        #cooked_image = imageops.rotate(cooked_image, rotation_angle)
+
+        return cooked_image
+
+    def find_stars(self, image):
+        """Display image and track manual selection of stars"""
+
+        #az, el, i, j, raw_file = self.parse_file(star_cal_file)
+
+        self.prep_star_lookup()
+
+        self.star_hip = list()
+        self.star_azel = list()
+        self.star_pos = list()
 
         # Display image with stars
-        fig = plt.figure(figsize=(15,10))
-        ax = fig.add_subplot(111)
-        ax.imshow(cooked_image, cmap='gray')
-        ax.scatter(i, j, facecolors='none', edgecolors='r')
-
-        # Setup clicker object to keep track of identified stars
-        self.klicker = clicker(ax, ['stars'])
-        self.star_hip = list()
-        self.klicker.on_point_added(self.add_star)
-
+        self.fig, self.ax = plt.subplots()
+        # Set up button press event trigger
+        self.fig.canvas.mpl_connect('button_press_event', self.add_star)
+        # Display image
+        self.ax.imshow(image, cmap='gray')
         plt.show()
 
-        self.star_pos = self.klicker.get_positions()['stars']
-       
 
-    def lookup_stars(self):
-        """Look up star position"""
+    def prep_star_lookup(self):
+        """Prepare skyfield for star lookups"""
         ts = load.timescale()
         t = ts.utc(self.time.year,self.time.month,self.time.day,self.time.hour,self.time.minute,self.time.second)
         planets = load('de421.bsp')
         earth = planets['earth']
         site = earth + wgs84.latlon(self.site_lat, self.site_lon, elevation_m=0)
-        site_ref = site.at(t)
+        self.site_ref = site.at(t)
 
         with load.open(hipparcos.URL) as f:
-            df = hipparcos.load_dataframe(f)
-
-        self.star_azel = list()
-        for hip in self.star_hip:
-            s = Star.from_dataframe(df.loc[float(hip)])
-
-            elev, azmt, _ = site_ref.observe(s).apparent().altaz()
-            self.star_azel.append([azmt.degrees, elev.degrees])
+            self.df = hipparcos.load_dataframe(f)
 
 
-    def save_starcal_file(self, output, star_cal_file):
+    def save_starcal_file(self, output):
         """ Save output starcal file"""
 
         with open(output, 'w') as f:
-            # copy existing file
-            f.write(star_cal_file)
+            # write header
+            f.write(f'# {self.site_station.upper()}    {self.site_instrument}\n')
+            f.write(f'# {self.time.isoformat()}\n')
+            f.write(f'# GLAT={self.site_lat:10.6f}    GLON={self.site_lon:10.6f}\n')
+            f.write(80*'#'+'\n\n')
+            f.write(f'# {"HIP":8}{"Azimuth":>20}{"Elevation":>20}{"X":>15}{"Y":>15}\n')
 
             # add new stars
             for hip, azel, pos in zip(self.star_hip, self.star_azel, self.star_pos):
-                f.write(f'{hip}    {azel[0]}    {azel[1]}    {pos[0]}    {pos[1]}\n')
+                f.write(f'{hip:10}{azel[0]:20.10f}{azel[1]:20.10f}{pos[0]:15.5f}{pos[1]:15.5f}\n')
 
     def parse_file(self, star_cal_file):
         """Read starcal file"""
@@ -128,6 +160,38 @@ class StarCal:
         az, el, i, j = np.loadtxt(io.StringIO(star_cal_file), usecols=(1,2,3,4), unpack=True)
 
         return az, el, i, j, raw_filename
+
+
+
+def equalize(image, contrast, num_bins=10000):
+    """Histogram Equalization to adjust contrast [1%-99%]"""
+    # copied function from imageops.py
+    # needed to make the image visable - there may be more efficient ways of doing this
+
+    image_array_1d = image.flatten()
+
+    image_histogram, bins = np.histogram(image_array_1d, num_bins)
+    image_histogram = image_histogram[1:]
+    bins = bins[1:]
+    cdf = np.cumsum(image_histogram)
+
+    # spliced to cut off non-image area
+    # any way to determine this dynamically?  How periminant is it?
+    cdf = cdf[:9996]
+
+    max_cdf = max(cdf)
+    max_index = np.argmin(abs(cdf - contrast / 100 * max_cdf))
+    min_index = np.argmin(abs(cdf - (100 - contrast) / 100 * max_cdf))
+    vmax = float(bins[max_index])
+    vmin = float(bins[min_index])
+    low_value_indices = image_array_1d < vmin
+    image_array_1d[low_value_indices] = vmin
+    high_value_indices = image_array_1d > vmax
+    image_array_1d[high_value_indices] = vmax
+
+    return image_array_1d.reshape(image.shape)
+
+
 
 
 
@@ -167,7 +231,21 @@ def find_starcal(station, instrument):
 
     logging.debug("Using package starcal file: %s", starcal_file)
 
-    return resources.files("mangonetwork.raw.data").joinpath(starcal_file).read_text()
+    #return resources.files("mangonetwork.raw.data").joinpath(starcal_file).read_text()
+    return starcal_file
+
+def download_image(station, instrument):
+    """Download image for star matching"""
+
+    dtstr = input('Enter date-time (YYYY-MM-DD HH:MM:SS): ')
+    time = dt.datetime.fromisoformat(dtstr)
+    print(time)
+
+    url = f'https://data.mangonetwork.org/data/transport/mango/archive/{station.lower()}/{instrument}/raw/{time:%Y}/{time:%j}/{time:%H}/mango-{station.lower()}-{instrument}-{time:%Y%m%d-%H%M%S}.hdf5'
+    print(url)
+    r=requests.get(url)
+    open('mango_image.hdf5', 'wb').write(r.content)
+    return 'mango_image.hdf5'
 
 
 def main():
@@ -190,7 +268,9 @@ def main():
     else:
         contents = find_starcal(args.station, args.instrument)
 
-    StarCal(contents, args.output)
+    image_filename = download_image(args.station, args.instrument)
+    #StarCal(contents, args.output)
+    StarCal(image_filename, args.output)
 
     sys.exit(0)
 
