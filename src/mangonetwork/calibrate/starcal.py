793366e7e9ed66247a38d178625bdaf9ef96751c
Leslie Lamarche <leslie.lamarche@sri.com>
2026-09-04 18:32:21 -0700

    Modify starcal.py to use generalized asistarcalibration package.

--- src/mangonetwork/calibrate/starcal.py
+++ src/mangonetwork/calibrate/starcal.py
@@ -35,6 +35,8 @@ from skyfield.api import Star, load, wgs84
 from skyfield.data import hipparcos
 from skyfield.named_stars import named_star_dict
 
+from asistarcalibration.starfinder import StarFinder
+
 
 #if sys.version_info < (3, 9):
 #    import importlib_resources as resources
@@ -42,166 +44,191 @@ from skyfield.named_stars import named_star_dict
 #    from importlib import resources
 
 
-class StarCal:
-    """Star calibration"""
+#class StarCal:
+#    """Star calibration"""
+#
+#    def __init__(self, image_file, output_file, sc_file=None):
+#
+#        image = self.load_image(image_file)
+#        self.find_stars(image, sc_file)
+#        self.save_starcal_file(output_file)
+#
+#
+#    def add_star(self, click):
+#        """Add user selected star and az,el based on HIP."""
+#        # This makes use of the Hipparcos Catolog
+#        # https://rhodesmill.org/skyfield/stars.html
+#
+#        # Star location in figure from click event
+#        x = click.xdata
+#        y = click.ydata
+#        print(f"Star at {x=:02f}, {y=:02f}")
+#
+#        # User entered HIP number
+#        hip = input('HIP #: ')
+#        hip = int(hip)
+#        print(hip)
+#
+#        # Look up star based on HIP and calculate az/el
+#        try:
+#            s = Star.from_dataframe(self.hipcat.loc[hip])
+#        except KeyError:
+#            print(f'Entered Hipparcos designation {hip} is not in database!')
+#            return
+#
+#        elev, azmt, _ = self.site_ref.observe(s).apparent().altaz()
+#
+#        # Append star information
+#        self.star_azel.append([azmt.degrees, elev.degrees])
+#        self.star_pos.append([x, y])
+#        self.star_hip.append(hip)
+#        self.star_name.append(self.hipcat.loc[hip,'name'])
+#
+#        # Mark star on plot
+#        self.ax.scatter(x, y, facecolors='none', edgecolors='r')
+#        self.fig.canvas.draw()
+#
+#    def load_image(self, raw_file):
+#        """Load image and metadata from raw file"""
+#
+#        image = h5py.File(raw_file, 'r')['image']
+#        cooked_image = self.prep_image(image)
+#
+#        self.time = dt.datetime.utcfromtimestamp(image.attrs['start_time'])
+#        self.site_lat = image.attrs['latitude']
+#        self.site_lon = image.attrs['longitude']
+#        self.site_station = image.attrs['station']
+#        self.site_instrument = image.attrs['instrument']
+#
+#        return cooked_image
+#
+#    def prep_image(self, image, contrast=99.95, rotation_angle=0.):
+#        """Prepare image to display"""
+#
+#        cooked_image = np.array(image)
+#        cooked_image = equalize(cooked_image, contrast)
+#
+#        return cooked_image
+#
+#    def find_stars(self, image, sc_file=None):
+#        """Display image and track manual selection of stars"""
+#
+#        self.prep_star_lookup()
+#
+#        print('Site Information\n'+16*'=')
+#        print(f'{self.site_station.upper()}    {self.site_instrument}')
+#        print(f'TIME: {self.time}')
+#        print(f'GLAT: {self.site_lat}\nGLON: {self.site_lon}')
+#
+#        # Initialize arrays
+#        self.star_name = list()
+#        self.star_hip = list()
+#        self.star_azel = list()
+#        self.star_pos = list()
+#        if sc_file:
+#            with open(sc_file) as f:
+#                for line in f:
+#                    # Skip lines that are comments or blank
+#                    if (not line or line[0]=='#' or line.isspace()):
+#                        continue
+#                    name, hip, az, el, x, y = line.split()
+#                    self.star_name.append(name)
+#                    self.star_hip.append(int(hip))
+#                    self.star_azel.append([float(az), float(el)])
+#                    self.star_pos.append([float(x), float(y)])
+#            # Check stars included in the provided starcal file against hip catolog
+#            self.check_stars()
+#
+#        # Display image with stars
+#        self.fig, self.ax = plt.subplots()
+#        # Set up button press event trigger
+#        self.fig.canvas.mpl_connect('button_press_event', self.add_star)
+#        # Display image
+#        self.ax.imshow(image, cmap='gray')
+#        
+#        for x, y in self.star_pos:
+#            self.ax.scatter(x, y, facecolors='none', edgecolors='r')
+#
+#        plt.show()
+#
+#
+#    def prep_star_lookup(self):
+#        """Prepare skyfield for star lookups"""
+#
+#        # Define site location
+#        ts = load.timescale()
+#        t = ts.utc(self.time.year,self.time.month,self.time.day,self.time.hour,self.time.minute,self.time.second)
+#        planets = load('de421.bsp')
+#        earth = planets['earth']
+#        site = earth + wgs84.latlon(self.site_lat, self.site_lon, elevation_m=0)
+#        self.site_ref = site.at(t)
+#
+#        # Load HIP catolog
+#        with load.open(hipparcos.URL) as f:
+#            df = hipparcos.load_dataframe(f)
+#
+#        # Add star names to HIP catolog
+#        star_name_list = ['xxxxx']*len(df)
+#        df['name'] = star_name_list
+#        for name, hip in named_star_dict.items():
+#            df.loc[hip,'name'] = name
+#
+#        #filtered_df = df[df['name'] != 'xxxxx']
+#        #print(filtered_df)
+#        self.hipcat = df
+#
+#    def check_stars(self):
+#        """Check stars loaded from input starcal file against HIP catalog for consistency"""
+#
+#        for name, hip, azel in zip(self.star_name, self.star_hip, self.star_azel):
+#            print('\nHIP: ', hip)
+#            print('Name: ', self.hipcat.loc[hip]['name'], name)
+#            s = Star.from_dataframe(self.hipcat.loc[hip])
+#            elev, azmt, _ = self.site_ref.observe(s).apparent().altaz()
+#            print('Azimuth: ', azmt.degrees, azel[0], azmt.degrees-azel[0])
+#            print('Elevation: ', elev.degrees, azel[1], elev.degrees-azel[1])
+#
+#
+#    def save_starcal_file(self, output):
+#        """ Save output starcal file"""
+#
+#        with open(output, 'w') as f:
+#            # write header
+#            f.write(f'# {self.site_station.upper()}    {self.site_instrument}\n')
+#            f.write(f'# {self.time.isoformat()}\n')
+#            f.write(f'# GLAT={self.site_lat:12.6f}    GLON={self.site_lon:12.6f}\n')
+#            f.write(80*'#'+'\n\n')
+#            f.write(f'# {"Name":<20}{"HIP":>8}{"Azimuth":>15}{"Elevation":>15}{"X":>10}{"Y":>10}\n')
+#
+#            # add new stars
+#            for name, hip, azel, pos in zip(self.star_name, self.star_hip, self.star_azel, self.star_pos):
+#                f.write(f'{name:20s}{hip:10d}{azel[0]:15.4f}{azel[1]:15.4f}{pos[0]:10.2f}{pos[1]:10.2f}\n')
+#
 
-    def __init__(self, image_file, output_file, sc_file=None):
-
-        image = self.load_image(image_file)
-        self.find_stars(image, sc_file)
-        self.save_starcal_file(output_file)
 
 
-    def add_star(self, click):
-        """Add user selected star and az,el based on HIP."""
-        # This makes use of the Hipparcos Catolog
-        # https://rhodesmill.org/skyfield/stars.html
+def load_image(raw_file):
+    """Load image and metadata from raw file"""
 
-        # Star location in figure from click event
-        x = click.xdata
-        y = click.ydata
-        print(f"Star at {x=:02f}, {y=:02f}")
+    image = h5py.File(raw_file, 'r')['image']
+    cooked_image = prep_image(image)
 
-        # User entered HIP number
-        hip = input('HIP #: ')
-        hip = int(hip)
-        print(hip)
+    time = dt.datetime.utcfromtimestamp(image.attrs['start_time'])
+    site_lat = image.attrs['latitude']
+    site_lon = image.attrs['longitude']
+    site_station = image.attrs['station']
+    site_instrument = image.attrs['instrument']
 
-        # Look up star based on HIP and calculate az/el
-        try:
-            s = Star.from_dataframe(self.hipcat.loc[hip])
-        except KeyError:
-            print(f'Entered Hipparcos designation {hip} is not in database!')
-            return
-
-        elev, azmt, _ = self.site_ref.observe(s).apparent().altaz()
-
-        # Append star information
-        self.star_azel.append([azmt.degrees, elev.degrees])
-        self.star_pos.append([x, y])
-        self.star_hip.append(hip)
-        self.star_name.append(self.hipcat.loc[hip,'name'])
-
-        # Mark star on plot
-        self.ax.scatter(x, y, facecolors='none', edgecolors='r')
-        self.fig.canvas.draw()
-
-    def load_image(self, raw_file):
-        """Load image and metadata from raw file"""
-
-        image = h5py.File(raw_file, 'r')['image']
-        cooked_image = self.prep_image(image)
-
-        self.time = dt.datetime.utcfromtimestamp(image.attrs['start_time'])
-        self.site_lat = image.attrs['latitude']
-        self.site_lon = image.attrs['longitude']
-        self.site_station = image.attrs['station']
-        self.site_instrument = image.attrs['instrument']
-
-        return cooked_image
-
-    def prep_image(self, image, contrast=99.95, rotation_angle=0.):
-        """Prepare image to display"""
-
-        cooked_image = np.array(image)
-        cooked_image = equalize(cooked_image, contrast)
-
-        return cooked_image
-
-    def find_stars(self, image, sc_file=None):
-        """Display image and track manual selection of stars"""
-
-        self.prep_star_lookup()
-
-        print('Site Information\n'+16*'=')
-        print(f'{self.site_station.upper()}    {self.site_instrument}')
-        print(f'TIME: {self.time}')
-        print(f'GLAT: {self.site_lat}\nGLON: {self.site_lon}')
-
-        # Initialize arrays
-        self.star_name = list()
-        self.star_hip = list()
-        self.star_azel = list()
-        self.star_pos = list()
-        if sc_file:
-            with open(sc_file) as f:
-                for line in f:
-                    # Skip lines that are comments or blank
-                    if (not line or line[0]=='#' or line.isspace()):
-                        continue
-                    name, hip, az, el, x, y = line.split()
-                    self.star_name.append(name)
-                    self.star_hip.append(int(hip))
-                    self.star_azel.append([float(az), float(el)])
-                    self.star_pos.append([float(x), float(y)])
-            # Check stars included in the provided starcal file against hip catolog
-            self.check_stars()
-
-        # Display image with stars
-        self.fig, self.ax = plt.subplots()
-        # Set up button press event trigger
-        self.fig.canvas.mpl_connect('button_press_event', self.add_star)
-        # Display image
-        self.ax.imshow(image, cmap='gray')
-        
-        for x, y in self.star_pos:
-            self.ax.scatter(x, y, facecolors='none', edgecolors='r')
-
-        plt.show()
+    return cooked_image, time, site_lat, site_lon, site_station, site_instrument
 
 
-    def prep_star_lookup(self):
-        """Prepare skyfield for star lookups"""
+def prep_image(image, contrast=99.95, rotation_angle=0.):
+    """Prepare image to display"""
 
-        # Define site location
-        ts = load.timescale()
-        t = ts.utc(self.time.year,self.time.month,self.time.day,self.time.hour,self.time.minute,self.time.second)
-        planets = load('de421.bsp')
-        earth = planets['earth']
-        site = earth + wgs84.latlon(self.site_lat, self.site_lon, elevation_m=0)
-        self.site_ref = site.at(t)
+    cooked_image = np.array(image)
+    cooked_image = equalize(cooked_image, contrast)
 
-        # Load HIP catolog
-        with load.open(hipparcos.URL) as f:
-            df = hipparcos.load_dataframe(f)
-
-        # Add star names to HIP catolog
-        star_name_list = ['xxxxx']*len(df)
-        df['name'] = star_name_list
-        for name, hip in named_star_dict.items():
-            df.loc[hip,'name'] = name
-
-        #filtered_df = df[df['name'] != 'xxxxx']
-        #print(filtered_df)
-        self.hipcat = df
-
-    def check_stars(self):
-        """Check stars loaded from input starcal file against HIP catalog for consistency"""
-
-        for name, hip, azel in zip(self.star_name, self.star_hip, self.star_azel):
-            print('\nHIP: ', hip)
-            print('Name: ', self.hipcat.loc[hip]['name'], name)
-            s = Star.from_dataframe(self.hipcat.loc[hip])
-            elev, azmt, _ = self.site_ref.observe(s).apparent().altaz()
-            print('Azimuth: ', azmt.degrees, azel[0], azmt.degrees-azel[0])
-            print('Elevation: ', elev.degrees, azel[1], elev.degrees-azel[1])
-
-
-    def save_starcal_file(self, output):
-        """ Save output starcal file"""
-
-        with open(output, 'w') as f:
-            # write header
-            f.write(f'# {self.site_station.upper()}    {self.site_instrument}\n')
-            f.write(f'# {self.time.isoformat()}\n')
-            f.write(f'# GLAT={self.site_lat:12.6f}    GLON={self.site_lon:12.6f}\n')
-            f.write(80*'#'+'\n\n')
-            f.write(f'# {"Name":<20}{"HIP":>8}{"Azimuth":>15}{"Elevation":>15}{"X":>10}{"Y":>10}\n')
-
-            # add new stars
-            for name, hip, azel, pos in zip(self.star_name, self.star_hip, self.star_azel, self.star_pos):
-                f.write(f'{name:20s}{hip:10d}{azel[0]:15.4f}{azel[1]:15.4f}{pos[0]:10.2f}{pos[1]:10.2f}\n')
-
+    return cooked_image
 
 
 def equalize(image, contrast, num_bins=10000):
@@ -345,7 +372,14 @@ def main():
     image_filename = download_image(station, instrument, time)
 
     # Run star calibration
-    StarCal(image_filename, args.output, sc_file=starcal_file)
+    #StarCal(image_filename, args.output, sc_file=starcal_file)
+
+    img, time, site_lat, site_lon, site_station, site_instrument = load_image(image_filename)
+
+    find = StarFinder(site_lat, site_lon, time)
+    find.find_stars(img)
+    find.save_starcal_file(args.output)
+
 
     sys.exit(0)
 
